4ca20ed7e5233cd4cd0049d04a4b4ac0e993a1ae
Christian M <chriamue@gmail.com>
2020-04-04 11:12:54 +0200

    added wallet to lib

---
+++ corona-diary-js/src/tests/wallet.test.ts
@@ -0,0 +1,42 @@
+import { Wallet } from '../wallet';
+
+test('sing', async () => {
+
+    const aliceWallet = new Wallet();
+    const bobWallet = new Wallet()
+
+    await aliceWallet.generate();
+    await bobWallet.generate();
+
+    expect(aliceWallet.getPrivateKey() == bobWallet.getPrivateKey()).toBeFalsy();
+
+    const alicePubKey = aliceWallet.getPublicKey();
+
+    const aliceMessage = 'Hello Bob!';
+    const aliceSignedMessage = await aliceWallet.sign(aliceMessage);
+
+    const aliceWalletAtBob = new Wallet();
+    aliceWalletAtBob.import(alicePubKey);
+    expect(aliceWalletAtBob.verify(aliceMessage, aliceSignedMessage)).toBeTruthy()
+
+})
+
+test('encrypt', async() =>{
+    const aliceWallet = new Wallet();
+    const bobWallet = new Wallet()
+
+    aliceWallet.generate();
+    bobWallet.generate();
+
+    expect(aliceWallet.getPrivateKey() == bobWallet.getPrivateKey()).toBeFalsy();
+
+    const alicePubKey = aliceWallet.getPublicKey();
+
+    const aliceWalletAtBob = new Wallet();
+    aliceWalletAtBob.import(alicePubKey);
+
+    const bobMessage = 'Hello Alice!';
+    const aliceEncryptedMessage = await aliceWalletAtBob.encrypt(bobMessage);
+    const aliceMessage = await aliceWallet.decrypt(aliceEncryptedMessage)
+    expect(aliceMessage).toEqual(bobMessage);
+})
---
+++ corona-diary-js/src/wallet.ts
@@ -0,0 +1,43 @@
+import RSA from 'node-rsa';
+
+export class Wallet {
+
+    bits: number = 1024;
+    key = new RSA();
+
+    constructor() {
+    }
+
+    generate() {
+        this.key.generateKeyPair(this.bits)
+    }
+
+    import(keyData: string) {
+        this.key = new RSA(keyData);
+    }
+
+    async encrypt(data: any) {
+        return this.key.encrypt(data, 'base64');
+    }
+
+    async decrypt(data: any) {
+        return this.key.decrypt(data, 'utf8');
+    }
+
+    async sign(data: any) {
+        return this.key.sign(data);
+    }
+
+    async verify(data: any, signature: any) {
+        return this.key.verify(data, signature);
+    }
+
+    getPublicKey() {
+        return this.key.exportKey('pkcs1-public-pem');
+    }
+
+    getPrivateKey() {
+        return this.key.exportKey('pkcs1-pem');
+    }
+
+}
